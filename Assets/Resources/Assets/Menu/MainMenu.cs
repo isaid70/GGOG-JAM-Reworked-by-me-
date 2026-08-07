4a324fdd7cfb6e571ad6ce550316aa2a90472e7f
Nagini <emresekizsu@gmail.com>
2026-08-08 02:24:42 +0300

    Ses ve Entegre Fix

--- Assets/Resources/Assets/Menu/MainMenu.cs
+++ Assets/Resources/Assets/Menu/MainMenu.cs
@@ -3,12 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("UI & Sahne Ayarları")]
     [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private string gameSceneName = "MainScene";
 
     public void PlayGame()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainScene");
+        if (!string.IsNullOrEmpty(gameSceneName))
+        {
+            SceneManager.LoadScene(gameSceneName);
+        }
+        else
+        {
+            Debug.LogError("[MainMenu] gameSceneName boş! Lütfen Inspector'da geçilecek sahne adını girin.");
+        }
     }
 
     public void OpenOptions()
